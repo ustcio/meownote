f58772b7559742a1a4f7bbd75e1cd7f2950450f0
ustcio <ustcio@users.noreply.github.com>
2026-04-12 00:08:01 +0800

    Fix workspace tag migration numbering

--- migrations/006_workspace_file_tags.sql
+++ migrations/006_workspace_file_tags.sql
@@ -2,3 +2,6 @@
 -- 为 workspace_files 增加 tag 字段
 
 ALTER TABLE workspace_files ADD COLUMN tag TEXT DEFAULT '';
+
+INSERT OR IGNORE INTO schema_migrations (version, applied_at)
+VALUES ('006', datetime('now'));
